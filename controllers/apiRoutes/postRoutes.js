023aa2a592563522771518a80613b99771318e8d
Kelvin Chao <kelvinchao2011@gmail.com>
2022-05-20 00:29:05 -0700

    minor update

--- controllers/apiRoutes/postRoutes.js
+++ controllers/apiRoutes/postRoutes.js
@@ -1,14 +1,46 @@
 const router = require("express").Router();
 const { User, Post, Comment } = require('../../models') 
 
-router.get("/", (req, res) => {})
+router.get("/", (req, res) => {
+  Post.findAll({
+    attributes: [
+      'id',
+      'title',
+      'text'
+    ],
+    include: [
+      {
+        model: 'User',
+        attributes: ['username']
+      }
+    ]
+  })
+    .then()
+    .catch()
+})
 
-router.get("/:id", (req, res) => {})
+router.get("/:id", (req, res) => {
+  Post.findOne()
+    .then()
+    .catch()
+})
 
-router.post("/", (req, res) => {})
+router.post("/", (req, res) => {
+  Post.create()
+    .then()
+    .catch()
+})
 
-router.put("/", (req, res) => {})
+router.put("/", (req, res) => {
+  Post.update()
+    .then()
+    .catch()
+})
 
-router.delete("/", (req, res) => {})
+router.delete("/", (req, res) => {
+  Post.destroy()
+    .then()
+    .catch()
+})
 
 module.exports = router;
